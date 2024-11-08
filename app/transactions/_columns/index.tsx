b02428981b031c5a830dbb02558dc6e39e37eea9
alemcar <sandromuniz27@hotmail.com>
2024-11-08 19:09:32 -0400

    feat: create add transaction dialog

--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -1,8 +1,15 @@
+"use client";
+
 import { Transaction } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
 import TransactionTypeBadge from "../_components/type-badge";
-// import { Button } from "@/app/_components/ui/button";
-// import { TrashIcon } from "lucide-react";
+import { Button } from "@/app/_components/ui/button";
+import { TrashIcon } from "lucide-react";
+import {
+  TRANSACTION_CATEGORY_LABELS,
+  TRANSACTION_PAYMENT_METHOD_LABELS,
+} from "@/app/_constants/transactions";
+import EditTransactionButton from "../_components/edit-transaction-button";
 
 export const transactionColumns: ColumnDef<Transaction>[] = [
   {
@@ -19,11 +26,14 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "category",
     header: "Categoria",
-    cell: ({ row: { original: transaction } }) => transaction,
+    cell: ({ row: { original: transaction } }) =>
+      TRANSACTION_CATEGORY_LABELS[transaction.category],
   },
   {
     accessorKey: "paymentMethod",
     header: "Método de Pagamento",
+    cell: ({ row: { original: transaction } }) =>
+      TRANSACTION_PAYMENT_METHOD_LABELS[transaction.paymentMethod],
   },
   {
     accessorKey: "date",
@@ -47,15 +57,15 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "actions",
     header: "Ações",
-    // cell: ({ row: { original: transaction } }) => {
-    //   return (
-    //     <div className="space-x-1">
-
-    //       <Button variant="ghost" size="icon" className="text-muted-foreground">
-    //         <TrashIcon />
-    //       </Button>
-    //     </div>
-    //   );
-    // },
+    cell: ({ row: { original: transaction } }) => {
+      return (
+        <div className="space-x-1">
+          <EditTransactionButton transaction={transaction} />
+          <Button variant="ghost" size="icon" className="text-muted-foreground">
+            <TrashIcon />
+          </Button>
+        </div>
+      );
+    },
   },
 ];
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,3 +1,4 @@
+import AddTransactionButton from "../_components/add-transaction-button";
 import { DataTable } from "../_components/ui/data-table";
 import { db } from "../_lib/prisma";
 import { transactionColumns } from "./_columns";
@@ -10,6 +11,7 @@ const TransactionsPage = async () => {
       {/* TÍTULO E BOTÃO */}
       <div className="flex w-full items-center justify-between">
         <h1 className="text-2xl font-bold">Transações</h1>
+        <AddTransactionButton />
       </div>
 
       <DataTable columns={transactionColumns} data={transactions} />
