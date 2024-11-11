e8f7c4488b02baa8bd39a7bceb261999dc495105
alemcar <sandromuniz27@hotmail.com>
2024-11-11 14:36:01 -0400

    feat: add transaction pie chart component

--- app/(home)/_components/summary-cards.tsx
+++ app/(home)/_components/summary-cards.tsx
@@ -1,3 +1,11 @@
+import {
+  PiggyBankIcon,
+  TrendingDownIcon,
+  TrendingUpIcon,
+  WalletIcon,
+} from "lucide-react";
+import SummaryCard from "./summary-card";
+
 interface SummaryCards {
   month: string;
   balance: number;
@@ -13,8 +21,35 @@ const SummaryCards = async ({
   investmentsTotal,
 }: SummaryCards) => {
   return (
-    <div>
-      {balance}, {depositsTotal}, {expensesTotal}, {investmentsTotal}{" "}
+    <div className="space-y-6">
+      {/* PRIMEIRO CARD */}
+      <SummaryCard
+        icon={<WalletIcon size={16} />}
+        title="Saldo"
+        amount={balance}
+        size="large"
+      />
+
+      {/* OUTROS CARDS */}
+      <div className="grid grid-cols-3 gap-6">
+        <SummaryCard
+          icon={<PiggyBankIcon size={16} />}
+          title="Investido"
+          amount={investmentsTotal}
+        />
+
+        <SummaryCard
+          icon={<TrendingUpIcon size={16} className="text-primary" />}
+          title="Receita"
+          amount={depositsTotal}
+        />
+
+        <SummaryCard
+          icon={<TrendingDownIcon size={16} className="text-red-500" />}
+          title="Despesas"
+          amount={expensesTotal}
+        />
+      </div>
     </div>
   );
 };
